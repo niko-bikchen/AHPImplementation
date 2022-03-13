9e0039bafce9aa1c6aefec8b95c0a20e6ff627d4
Nikolay Bikchentaev <niko.bikcheb@gmail.com>
2022-03-13 20:48:34 +0200

    Changed folder structure.

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from os import path
 from flask import Flask, send_from_directory
 
 
-app = Flask(__name__, static_url_path="/build",
-            static_folder='build')
+app = Flask(__name__)
 
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(path.join(app.root_path, 'build'),
+    return send_from_directory(path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/x-icon')
 
 
 @app.route('/', methods=['GET'])
 def getIndex() -> 'html':
-    return app.send_static_file('index.html')
+    return app.send_static_file('html/index.html')
